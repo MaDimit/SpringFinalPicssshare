35d723ebd4aaf1fed1cf111f7fc495c52e7268d1
Maxim D <mdimirtov@gmail.com>
2018-04-26 16:19:08 +0300

    Working spring project

--- src/main/java/project/Application.java
+++ src/main/java/project/Application.java
@@ -1,5 +1,6 @@
 package project;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +22,7 @@ import javax.sql.DataSource;
 @Configuration
 @ComponentScan("project")
 @EnableAutoConfiguration
+@EnableConfigurationProperties
 @SpringBootApplication
 public class Application extends WebMvcAutoConfiguration {
 
@@ -27,15 +30,23 @@ public class Application extends WebMvcAutoConfiguration {
         SpringApplication.run(Application.class, args);
     }
 
+    @Value("${spring.datasource.url}") String jdbcUrl;
+    @Value("${spring.datasource.username}") String jdbcUser;
+    @Value("${spring.datasource.password}") String jdbcPassword;
+    @Value("${spring.datasource.driver-class-name}") String jdbcDriver;
+
     @Bean
     public DataSource dataSource() {
+
         return DataSourceBuilder
                 .create()
-                .username("root")
-                .password("root")
-                .url("jdbc:mysql://127.0.0.1:3306/picssshare?autoReconnect=true&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC")
-                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url(jdbcUrl)
+                .password(jdbcPassword)
+                .username(jdbcUser)
+                .driverClassName(jdbcDriver)
                 .build();
     }
+
+
 }
 
